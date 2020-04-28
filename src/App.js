1b9bc01c6850ff7984afe1579688b00b3f741284
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-28 03:04:57 +0300

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ConfirmCode from './ConfirmCode';
 import './App.css';
 
 function App() {
-  const [confirmPage, setConfirmPage] = useState(true);
-  if (confirmPage)
-    return (
-      <div className="App">
-        <br />
-        <br />
-        <ConfirmCode />
-      </div>
-    );
   return (
     <div className="App">
-      <input type="text" />
-      <input type="text" />
-      <input type="button" value="gir" onClick={() => setConfirmPage(true)} />
-      {/*  <div style={{display:confirmPage ? 'block' :'none'}}>
-        <ConfirmCode/>
-      </div> */}
+      <br />
+      <br />
+      <ConfirmCode />
     </div>
   );
 }
