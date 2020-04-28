c530c6a41727589bc365fd6aa174dcbf3d0b31dd
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-28 03:07:23 +0300

    Updates

---
+++ precache-manifest.0bc017b817f9ce64d1ec260eaf44566f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cef7aee5619d8eeb2778aca923f47fc",
+    "url": "/confirmcode/index.html"
+  },
+  {
+    "revision": "1e311d4bcaf4c7e119b2",
+    "url": "/confirmcode/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a5d83995a768b942847b",
+    "url": "/confirmcode/static/js/2.fb802083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/confirmcode/static/js/2.fb802083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e311d4bcaf4c7e119b2",
+    "url": "/confirmcode/static/js/main.0fcece2a.chunk.js"
+  },
+  {
+    "revision": "55f61011133d9bd0d69c",
+    "url": "/confirmcode/static/js/runtime-main.0ff57a09.js"
+  }
+]);
